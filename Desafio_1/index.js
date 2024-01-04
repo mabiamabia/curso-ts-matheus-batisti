3fa8a24613ce90cba78e501f338b914863d55640
priscila.costa <priscila.costa@g.globo>
2024-01-03 21:45:25 -0300

    feat: resolve challenge

---
+++ Desafio_1/index.js
@@ -0,0 +1,7 @@
+var x = 4;
+var y = 3;
+function soma(num1, num2) {
+    console.log(x * y);
+    return;
+}
+soma(x, y);
---
+++ Desafio_1/index.ts
@@ -0,0 +1,10 @@
+const x = 4;
+const y = 3;
+
+function soma(num1: number, num2: number): number {
+    const resultado = num1 * num2;
+    console.log(resultado);
+    return resultado;
+}
+
+soma(x, y);
